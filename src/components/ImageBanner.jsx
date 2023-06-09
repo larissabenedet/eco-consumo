404ee1e05d55c1c116660ce502d87d4df86e53c3
laribenedet <larissa.benedet@portallacrei.com.br>
2023-06-09 14:02:11 -0300

    feat: banner

---
+++ src/components/ImageBanner.jsx
@@ -0,0 +1,19 @@
+const ImageBanner = () => {
+  return (
+    <section className="banner">
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi et
+        assumenda accusamus mollitia veniam officia libero inventore deserunt
+        ab! Et tempore eos id, mollitia possimus explicabo laudantium distinctio
+        illo nisi.
+      </h2>
+      <ul>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+      </ul>
+    </section>
+  );
+};
+
+export default ImageBanner;
