7966ff9fa547151be66ef39a036560ebad13de73
laribenedet <larissa.benedet@portallacrei.com.br>
2023-06-09 14:01:44 -0300

    feat: add project files

---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+const Cards = () => {
+  return (
+    <div>
+      <div className="imgCard">
+        <img
+          src="https://images.unsplash.com/photo-1570913196376-dacb677ef459?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80"
+          alt="Eco Image"
+        />
+      </div>
+      <div className="cardDescription">
+        <h3>Lorem ipsum</h3>
+        <h4>Lorem ipsum dolor sit amet consectetur adipiscing.</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
